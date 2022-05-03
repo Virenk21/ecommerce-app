8b5aac99815a5aef53458785c53d044862ba58e1
Virenk21 <viren.d222111@gmail.com>
2022-05-03 17:05:34 +0530

    completed navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,43 +3,34 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-white py-2 shadow-sm">
+         <div className="container">
+            <a className="navbar-brand fw-bold fs-4" href="#">LA COLLECTION</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">About us</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"></hr></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact us</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-    </ul>
-
-    <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-
-
-
+        </ul>
+            <div className="buttons">
+                <a href="" className="btn btn-outline-dark">
+                   <i className="fa fa-sign-in me-1"></i> Login</a>
+                    <a href="" className="btn btn-outline-dark ms-2">
+                   <i className="fa fa- me-1"></i> Sign up</a>
+            </div>
+  
     </div>
          </div>
          </nav>
