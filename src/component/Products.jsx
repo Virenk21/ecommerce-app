5a86dd2febec923c8d3a73d6ccf3cebc6a555ebc
Virenk21 <viren.d222111@gmail.com>
2022-05-04 00:21:41 +0530

    completed product folder

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -24,15 +26,79 @@ const Products = () => {
   }, []);
 
   const Loading = () => {
-    return <>Loading...</>;
+    return (
+      <>
+        <div className="col-md-3">
+          <Skeleton />
+        </div>
+        </>
+    );
+  };
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((x)=>x.category === cat);
+    setFilter(updatedList);
   };
   const ShowProducts = () => {
     return (
       <>
-        <div className="buttons">
-          <button className="btn btn-outline-dark">All</button>
-          <bnutton className="btn btn-outline-dark">Men's Clothing</bnutton>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Jewellery
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronics
+          </button>
         </div>
+        {filter.map((product) => {
+          return (
+            <>
+              <div className="col-md-3 mb-4">
+                <div className="card h-100 text-center p-4" key={product.id}>
+                  <img
+                    src={product.image}
+                    className="card-img-top"
+                    alt={product.title}
+                    height="250px"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">
+                      {product.title.substring(0, 12)}...
+                    </h5>
+                    <p className="card-text lead fw-bold">₹{product.price}</p>
+                    <a href="#" className="btn btn-outline-dark">
+                      Add to cart
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+        ;
       </>
     );
   };
