e8b13485fc62e62d1f469ca59b92406a3cdf2f7b
Virenk21 <viren.d222111@gmail.com>
2022-05-04 20:29:02 +0530

    cart under construction

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,50 +1,43 @@
 import React from "react";
-import { useState } from 'react';
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import Skeleton from 'react-loading-skeleton';
 import { delCart } from "../Redux/action/index";
 import { NavLink } from "react-router-dom";
 
-const Cart = () => {
-  const [loading, setLoading] = useState(false);
+const Cart = (product) => {
 
   const state = useSelector((state) => state.addCart);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
- 
-  const ShowProduct = (product) => {
+   const handleClose = (item) => {
+        dispatch(delCart(item))
+    }
+
+    const ShowProduct = (product) => {
     return (
       <div className="px-4 my-5 bg-light rounded-3" key={product.id}>
-        <div className="container py-4">
-          <button className="btn-close float-end" aria-label="Close"></button>
+        <div  className="container py-4">
+          <button onClick={()=>handleClose(product)} className="btn-close float-end" aria-label="Close"></button>
           <div className="row justify-content-center">
-            <div className="col-md-4">
+           <div className="col-md-6">
               <img src={product.image} alt={product.title} height="200px" width="180px"/>
-               </div>
-               <div className="col-md-4">
-                            <h3>{product.title}</h3>
+                </div>
+                                                        
                             <p className="lead fw-bold">₹{product.price}</p>
+                            <p className="lead">
+                        Rating {product.rating && product.rating.rate}
+                        <i className="fa fa-star"></i>
+                    </p>
                         </div>
                     </div>
                 </div>
-            </div>
+           
         );
     }
 
-
-const Loading = () =>{
-            return (
-                <>
-                <div className="col-md-6">
-                    <Skeleton height= {400}/>
-                </div>
-                </>
-            )
-            }
   return (
   <>
-  {loading ? <Loading/> : <ShowProduct />}
+  {<ShowProduct />}
   </>
   );
 };
