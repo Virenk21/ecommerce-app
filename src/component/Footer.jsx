d64c263674ba68e1f0c5abae51ab529dfb139ea5
Virenk21 <viren.d222111@gmail.com>
2022-05-11 00:41:40 +0530

    further updated footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <p>
         
         </p>
-      <p1> Viren Corporation © 2022 <br /> All rights reserved.</p1>
+      <p1> Viren Corporation © 2022 <br /> Retail-mart Pvt. Ltd. &nbsp; All rights reserved.</p1>
       
     </footer>
   )
