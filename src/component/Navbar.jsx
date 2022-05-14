fe47623119b22298192f81c67cb87eb71e4ff960
Virenk21 <viren.d222111@gmail.com>
2022-05-14 14:36:59 +0530

    updated sign in title

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         </ul>
             <div className="buttons">
                 <NavLink to="/login" className="btn btn-outline-dark">
-                   <i className="fa fa-sign-in me-1"></i> Login</NavLink>
+                   <i className="fa fa-sign-in me-1"></i> Sign in</NavLink>
                     <NavLink to="/sign up" className="btn btn-outline-dark ms-2">
                    <i className="fa fa-user-plus me-1"></i> Sign up</NavLink>
                    <NavLink to="/cart" className="btn btn-outline-dark ms-2">
